76e60934dcf43900909afb60d556389cb4e46c0a
minh hieu <hieuvip444@gmail.com>
2019-11-12 14:16:41 +0700

    commit 12-11

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,9 @@
 <?php
 // Add custom Theme Functions here
+function wpb_load_fa() {
+
+    wp_enqueue_style( 'wpb-fa', get_stylesheet_directory_uri() . '/font-awesome-4.7.0/css/font-awesome.min.css' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_load_fa' );
